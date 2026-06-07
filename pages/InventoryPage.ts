8f51aa04ec1a60da7c02eae192235a1c493cb85e
Radosław Ptak <19503328+radoptak@users.noreply.github.com>
2026-06-07 18:59:59 +0200

    test: add inventory sorting coverage (#7)
    * refactor: add inventory sorting action
    
    * refactor: add inventory product names reader
    
    * test: add inventory sorting coverage
    
    * test: add inventory sorting coverage

--- pages/InventoryPage.ts
+++ pages/InventoryPage.ts
@@ -4,19 +4,31 @@ export class InventoryPage {
   private readonly page: Page;
   readonly headerTitle: Locator;
   private readonly inventoryItems: Locator;
+  private readonly inventoryItemNames: Locator;
   private readonly shoppingCartBadge: Locator;
+  private readonly sortDropdown: Locator;
   readonly shoppingCartLink: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.headerTitle = page.getByTestId('title');
     this.inventoryItems = page.getByTestId('inventory-item');
+    this.inventoryItemNames = page.getByTestId('inventory-item-name');
     this.shoppingCartBadge = page.getByTestId('shopping-cart-badge');
+    this.sortDropdown = page.getByTestId('product-sort-container');
     this.shoppingCartLink = page.getByTestId('shopping-cart-link');
   }
 
   async getInventoryCount(): Promise<number> {
     return this.inventoryItems.count();
+  }
+
+  async sortProductsBy(option: string): Promise<void> {
+    await this.sortDropdown.selectOption(option);
+  }
+
+  async getProductNames(): Promise<string[]> {
+    return this.inventoryItemNames.allTextContents();
   }
 
   async addItemToCartById(productSlug: string): Promise<void> {
